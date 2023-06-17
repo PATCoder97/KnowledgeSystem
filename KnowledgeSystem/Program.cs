24484ec6cbd521a542d9c084094692566ac30be6
AnhTuanDELL <Phananhtuan6@outlook.com>
2023-06-17 08:43:31 +0700

    Add project files.

---
+++ KnowledgeSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.LookAndFeel;
+using DevExpress.Skins;
+using DevExpress.UserSkins;
+using KnowledgeSystem.Views._00_Generals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace KnowledgeSystem
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new fMain());
+        }
+    }
+}
---
+++ KnowledgeSystem/Views/00_Generals/fMain.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace KnowledgeSystem.Views._00_Generals
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 412);
+            this.Name = "fMain";
+            this.Text = "fMain";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
