74cdcf85c497733eade0579fbff5d8b799063d5d
AnhTuanDELL <Phananhtuan6@outlook.com>
2023-08-10 15:54:39 +0700

    show notify doc update

---
+++ KnowledgeSystem/Views/02_StandardsAndTechs/07_KnowledgeBase/uc207_Notify_DocUpdateNotify.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace KnowledgeSystem.Views._02_StandardsAndTechs._07_KnowledgeBase
+{
+    partial class uc207_Notify_DocUpdateNotify
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraGrid.GridFormatRule gridFormatRule1 = new DevExpress.XtraGrid.GridFormatRule();
+            DevExpress.XtraEditors.FormatConditionRuleExpression formatConditionRuleExpression1 = new DevExpress.XtraEditors.FormatConditionRuleExpression();
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gcData = new DevExpress.XtraGrid.GridControl();
+            this.gvData = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gvColId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn6 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gvColIdKnowledgeBase = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gcData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.AllowCustomization = false;
+            this.layoutControl1.Controls.Add(this.label1);
+            this.layoutControl1.Controls.Add(this.gcData);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.Root;
+            this.layoutControl1.Size = new System.Drawing.Size(912, 537);
+            this.layoutControl1.TabIndex = 1;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("DFKai-SB", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(888, 34);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "文件更新提示";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gcData
+            // 
+            this.gcData.Location = new System.Drawing.Point(12, 50);
+            this.gcData.MainView = this.gvData;
+            this.gcData.Name = "gcData";
+            this.gcData.Size = new System.Drawing.Size(888, 475);
+            this.gcData.TabIndex = 4;
+            this.gcData.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvData});
+            this.gcData.DoubleClick += new System.EventHandler(this.gcData_DoubleClick);
+            // 
+            // gvData
+            // 
+            this.gvData.Appearance.HeaderPanel.Font = new System.Drawing.Font("DFKai-SB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gvData.Appearance.HeaderPanel.ForeColor = System.Drawing.Color.Black;
+            this.gvData.Appearance.HeaderPanel.Options.UseFont = true;
+            this.gvData.Appearance.HeaderPanel.Options.UseForeColor = true;
+            this.gvData.Appearance.HeaderPanel.Options.UseTextOptions = true;
+            this.gvData.Appearance.HeaderPanel.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.gvData.Appearance.Row.Font = new System.Drawing.Font("DFKai-SB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gvData.Appearance.Row.ForeColor = System.Drawing.Color.Black;
+            this.gvData.Appearance.Row.Options.UseFont = true;
+            this.gvData.Appearance.Row.Options.UseForeColor = true;
+            this.gvData.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gvColId,
+            this.gridColumn6,
+            this.gvColIdKnowledgeBase,
+            this.gridColumn1,
+            this.gridColumn2,
+            this.gridColumn3});
+            gridFormatRule1.ApplyToRow = true;
+            gridFormatRule1.Name = "Format0";
+            formatConditionRuleExpression1.Appearance.ForeColor = System.Drawing.Color.Red;
+            formatConditionRuleExpression1.Appearance.Options.UseForeColor = true;
+            formatConditionRuleExpression1.Expression = "[IsRead] = False";
+            gridFormatRule1.Rule = formatConditionRuleExpression1;
+            this.gvData.FormatRules.Add(gridFormatRule1);
+            this.gvData.GridControl = this.gcData;
+            this.gvData.Name = "gvData";
+            this.gvData.OptionsSelection.EnableAppearanceHotTrackedRow = DevExpress.Utils.DefaultBoolean.True;
+            this.gvData.OptionsView.EnableAppearanceOddRow = true;
+            this.gvData.OptionsView.ShowAutoFilterRow = true;
+            this.gvData.OptionsView.ShowGroupPanel = false;
+            this.gvData.CustomDrawRowIndicator += new DevExpress.XtraGrid.Views.Grid.RowIndicatorCustomDrawEventHandler(this.gvData_CustomDrawRowIndicator);
+            // 
+            // gvColId
+            // 
+            this.gvColId.Caption = "gridColumn4";
+            this.gvColId.FieldName = "Id";
+            this.gvColId.Name = "gvColId";
+            // 
+            // gridColumn6
+            // 
+            this.gridColumn6.AppearanceCell.Options.UseTextOptions = true;
+            this.gridColumn6.AppearanceCell.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.gridColumn6.Caption = "時間";
+            this.gridColumn6.DisplayFormat.FormatString = "yyyy/MM/dd HH:mm";
+            this.gridColumn6.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
+            this.gridColumn6.FieldName = "TimeNotify";
+            this.gridColumn6.MaxWidth = 200;
+            this.gridColumn6.Name = "gridColumn6";
+            this.gridColumn6.Visible = true;
+            this.gridColumn6.VisibleIndex = 0;
+            // 
+            // gvColIdKnowledgeBase
+            // 
+            this.gvColIdKnowledgeBase.AppearanceCell.Options.UseTextOptions = true;
+            this.gvColIdKnowledgeBase.AppearanceCell.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.gvColIdKnowledgeBase.Caption = "資料編號";
+            this.gvColIdKnowledgeBase.FieldName = "IdKnowledgeBase";
+            this.gvColIdKnowledgeBase.MaxWidth = 200;
+            this.gvColIdKnowledgeBase.Name = "gvColIdKnowledgeBase";
+            this.gvColIdKnowledgeBase.Visible = true;
+            this.gvColIdKnowledgeBase.VisibleIndex = 1;
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "資料名稱";
+            this.gridColumn1.FieldName = "DisplayName";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 2;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "通知Notes時期";
+            this.gridColumn2.FieldName = "TimeNotyfiNotes";
+            this.gridColumn2.MaxWidth = 200;
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 3;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "IsRead";
+            this.gridColumn3.FieldName = "IsRead";
+            this.gridColumn3.Name = "gridColumn3";
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.layoutControlItem2});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(912, 537);
+            this.Root.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.gcData;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 38);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(892, 479);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.label1;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem2.MaxSize = new System.Drawing.Size(0, 38);
+            this.layoutControlItem2.MinSize = new System.Drawing.Size(24, 38);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(892, 38);
+            this.layoutControlItem2.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // uc207_Notify_DocUpdateNotify
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "uc207_Notify_DocUpdateNotify";
+            this.Size = new System.Drawing.Size(912, 537);
+            this.Load += new System.EventHandler(this.uc207_Notify_DocUpdateNotify_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gcData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraGrid.GridControl gcData;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvData;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn6;
+        private DevExpress.XtraGrid.Columns.GridColumn gvColIdKnowledgeBase;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraGrid.Columns.GridColumn gvColId;
+    }
+}
---
+++ KnowledgeSystem/Views/02_StandardsAndTechs/07_KnowledgeBase/uc207_Notify_DocUpdateNotify.cs
@@ -0,0 +1,130 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraGrid.Views.Grid;
+using KnowledgeSystem.Configs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Migrations;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnowledgeSystem.Views._02_StandardsAndTechs._07_KnowledgeBase
+{
+    public partial class uc207_Notify_DocUpdateNotify : DevExpress.XtraEditors.XtraUserControl
+    {
+        public uc207_Notify_DocUpdateNotify()
+        {
+            InitializeComponent();
+        }
+
+        Font fontIndicator = new Font("Times New Roman", 12.0f, FontStyle.Italic);
+        bool cal(Int32 _Width, GridView _View)
+        {
+            _View.IndicatorWidth = _View.IndicatorWidth < _Width ? _Width : _View.IndicatorWidth;
+            return true;
+        }
+
+        void IndicatorDraw(RowIndicatorCustomDrawEventArgs e)
+        {
+            e.Info.Appearance.Font = fontIndicator;
+            e.Info.Appearance.ForeColor = Color.FromArgb(16, 110, 190);
+        }
+
+        private void LoadData()
+        {
+            using (var db = new DBDocumentManagementSystemEntities())
+            {
+                var query = (from data in db.tbl207_NotifyEditDoc.Where(r => r.IdUserNotify == TempDatas.LoginId)
+                             join idDoc in db.dt207_DocProgress on data.IdDocProcess equals idDoc.Id
+                             join names in db.KnowledgeBases on idDoc.IdKnowledgeBase equals names.Id
+                             select new
+                             {
+                                 data.Id,
+                                 data.TimeNotify,
+                                 data.IsRead,
+                                 data.TimeNotyfiNotes,
+                                 idDoc.IdKnowledgeBase,
+                                 names.DisplayName
+                             }).ToList();
+
+                gcData.DataSource = query;
+            }
+
+            gvData.BestFitColumns();
+        }
+
+        private void uc207_Notify_DocUpdateNotify_Load(object sender, EventArgs e)
+        {
+            gvData.ReadOnlyGridView();
+
+            LoadData();
+        }
+
+        private void gvData_CustomDrawRowIndicator(object sender, RowIndicatorCustomDrawEventArgs e)
+        {
+            if (!gvData.IsGroupRow(e.RowHandle))
+            {
+                if (e.Info.IsRowIndicator)
+                {
+                    if (e.RowHandle < 0)
+                    {
+                        e.Info.ImageIndex = 0;
+                        e.Info.DisplayText = string.Empty;
+                    }
+                    else
+                    {
+                        e.Info.ImageIndex = -1;
+                        e.Info.DisplayText = (e.RowHandle + 1).ToString();
+                    }
+
+                    IndicatorDraw(e);
+
+                    SizeF size = e.Graphics.MeasureString(e.Info.DisplayText, fontIndicator);
+                    int width = (int)size.Width + 20;
+
+                    BeginInvoke(new MethodInvoker(() => cal(width, gvData)));
+                }
+            }
+            else
+            {
+                e.Info.ImageIndex = -1;
+                e.Info.DisplayText = $"[{e.RowHandle * -1}]";
+
+                IndicatorDraw(e);
+
+                SizeF size = e.Graphics.MeasureString(e.Info.DisplayText, fontIndicator);
+                int width = (int)size.Width + 20;
+
+                BeginInvoke(new MethodInvoker(() => cal(width, gvData)));
+            }
+        }
+
+        private void gcData_DoubleClick(object sender, EventArgs e)
+        {
+            string IdKnowledgeBase = gvData.GetFocusedRowCellValue(gvColIdKnowledgeBase).ToString();
+            int id = Convert.ToInt32(gvData.GetFocusedRowCellValue(gvColId));
+
+            using (var db = new DBDocumentManagementSystemEntities())
+            {
+                var notifyDocUpdate = db.tbl207_NotifyEditDoc.First(r => r.Id == id);
+
+                if (notifyDocUpdate != null)
+                {
+                    notifyDocUpdate.IsRead = true;
+
+                    db.tbl207_NotifyEditDoc.AddOrUpdate(notifyDocUpdate);
+                    db.SaveChanges();
+                }
+            }
+
+            f207_Document_Info document_Info = new f207_Document_Info(IdKnowledgeBase);
+            document_Info.ShowDialog();
+
+            LoadData();
+        }
+    }
+}
