ff0e736c211141317d8a4cb3026e0f278ce4be14
AnhTuanDELL <Phananhtuan6@outlook.com>
2023-08-15 09:41:05 +0700

    Xử lý lấy các Doc có quyền tìm kiếm

--- KnowledgeSystem/Views/02_StandardsAndTechs/07_KnowledgeBase/uc207_Search.cs
+++ KnowledgeSystem/Views/02_StandardsAndTechs/07_KnowledgeBase/uc207_Search.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -24,15 +25,6 @@ namespace KnowledgeSystem.Views._02_StandardsAndTechs._07_KnowledgeBase
         }
 
         #region parameters
-
-        const string SIMPLE_MODE = "基本";
-        const string ADVANCED_MODE = "全部";
-
-        List<User> lsUsers = new List<User>();
-        List<dt207_Base> lsKnowledgeBase = new List<dt207_Base>();
-        List<KnowledgeType> lsKnowledgeTypes = new List<KnowledgeType>();
-
-        int idType = 0;
 
         BindingSource sourceKnowledge = new BindingSource();
 
@@ -54,39 +46,108 @@ namespace KnowledgeSystem.Views._02_StandardsAndTechs._07_KnowledgeBase
 
         private void LoadData()
         {
+            string userLogin = TempDatas.LoginId;
+
+            List<User> lsUsers = new List<User>();
+            List<dt207_Base> lsKnowledgeBase = new List<dt207_Base>();
+            List<KnowledgeType> lsKnowledgeTypes = new List<KnowledgeType>();
+
+            // Kiểm tra xem có lấy Keyword để hiện lên view không
             bool IsSimple = checkUseKeyword.CheckState == CheckState.Unchecked;
 
+            // Lưu thông tin liên quan đến giao diện
             helper.SaveViewInfo();
+
             using (var db = new DBDocumentManagementSystemEntities())
             {
+                // Lấy danh sách Users, lsKnowledgeTypes, lsTypeHisGetFile từ cơ sở dữ liệu
                 var lsTypeHisGetFile = db.KnowledgeTypeHisGetFiles.ToList();
+                lsKnowledgeTypes = db.KnowledgeTypes.ToList();
+                lsUsers = db.Users.ToList();
+
+                // Gán các 說明 quyền vào class temp
                 TempDatas.typeViewFile = lsTypeHisGetFile.FirstOrDefault(r => r.Id == 1);
                 TempDatas.typeSaveFile = lsTypeHisGetFile.FirstOrDefault(r => r.Id == 2);
                 TempDatas.typePrintFile = lsTypeHisGetFile.FirstOrDefault(r => r.Id == 3);
 
-                lsKnowledgeTypes = db.KnowledgeTypes.ToList();
-                var knowledgeType = lsKnowledgeTypes.Where(r => r.DisplayName == Text).Select(r => r).FirstOrDefault();
-                if (knowledgeType != null) { idType = knowledgeType.Id; }
-
+                // Lấy danh sách các giá trị IdKnowledgeBase mà IsComplete là false
                 var lsIdBaseRemove = db.dt207_DocProgress.Where(r => !r.IsComplete).Select(r => r.IdKnowledgeBase).ToList();
-                var lsBaseRemove = db.dt207_Base.Where(r => !lsIdBaseRemove.Contains(r.Id)).ToList();
 
-                lsUsers = db.Users.ToList();
-                lsKnowledgeBase = db.dt207_Base.RemoveRange(lsBaseRemove)
-                    .Where(r => !r.IsDelete)
-                    .Select(r => new { r.Id, r.DisplayName, r.Keyword, r.UserRequest, r.IdTypes, r.UserUpload, r.UploadDate })
-                    .ToList().Select(r => new dt207_Base
-                    {
-                        Id = r.Id,
-                        DisplayName = r.DisplayName,
-                        Keyword = r.Keyword,
-                        UserRequest = r.UserRequest,
-                        IdTypes = r.IdTypes,
-                        UserUpload = r.UserUpload,
-                        UploadDate = r.UploadDate
-                    }).ToList();
+                //// Lấy danh sách các thực thể dt207_Base từ cơ sở dữ liệu mà Id không có trong lsIdBaseRemove
+                //var lsBaseRemove = db.dt207_Base.Where(r => !lsIdBaseRemove.Contains(r.Id)).ToList();
+
+                //// Lấy danh sách các thực thể dt207_Base từ cơ sở dữ liệu, loại bỏ các thực thể có trong lsBaseRemove
+                //// Chọn các thuộc tính cụ thể và tạo ra các thực thể dt207_Base mới cho danh sách mới
+                //lsKnowledgeBase = db.dt207_Base.RemoveRange(lsBaseRemove)
+                //     .Where(r => !r.IsDelete) // Không lấy các văn kiện đã xoá
+                //     .Select(r => new dt207_Base
+                //     {
+                //         Id = r.Id,
+                //         DisplayName = r.DisplayName,
+                //         Keyword = r.Keyword,
+                //         UserRequest = r.UserRequest,
+                //         IdTypes = r.IdTypes,
+                //         UserUpload = r.UserUpload,
+                //         UploadDate = r.UploadDate
+                //     })
+                //     .ToList();
+
+                //var query = db.dt207_Base.ToList();
+                //var query1 = db.dt207_Base.RemoveRange(lsBaseRemove).ToList();
+
+                // Danh sách Doc đưa lên hoặc yêu cầu
+                var lsDocUserUpload = db.dt207_Base.Where(r => r.UserUpload == userLogin || r.UserRequest == userLogin).ToList();
+                var lsIdDocUserUpload = lsDocUserUpload.Select(r => r.Id).ToList();
+
+                // Join group với GroupUser để lấy Prioritize
+                var lsGroupP = (from data in db.GroupUsers.ToList()
+                                join p in db.Groups.ToList() on data.IdGroup equals p.Id
+                                select new
+                                {
+                                    data.IdGroup,
+                                    data.IdUser,
+                                    p.Prioritize
+                                }).ToList();
+
+                // Danh sách mà user có quyền tìm kiếm
+                var lsCanSearchs = (from ks in db.KnowledgeSecurities.ToList()
+                                    join gu in lsGroupP on ks.IdGroup equals gu.IdGroup into gj
+                                    from subGu in gj.DefaultIfEmpty()
+                                    where ks.IdUser == userLogin || (subGu != null ? subGu.IdUser == userLogin : false)
+                                    select new
+                                    {
+                                        ks.IdKnowledgeBase,
+                                        ks.IdGroup,
+                                        ks.IdUser,
+                                        UserG = subGu?.IdUser,
+                                        ks.SearchInfo,
+                                        Prioritize = subGu?.Prioritize ?? -1
+                                    } into dtData
+                                    group dtData by dtData.IdKnowledgeBase into dtg
+                                    select new
+                                    {
+                                        dtg.Key,
+                                        dtg.OrderBy(r => r.Prioritize).FirstOrDefault()?.Prioritize,
+                                        dtg.OrderBy(r => r.Prioritize).FirstOrDefault()?.SearchInfo,
+                                        dtg.OrderBy(r => r.Prioritize).FirstOrDefault()?.IdUser,
+                                        dtg.OrderBy(r => r.Prioritize).FirstOrDefault()?.UserG,
+                                    }).ToList();
+
+                var lsIsCanSearchs = lsCanSearchs.Where(r => r.SearchInfo == true).Select(r => r.Key).ToList();
+
+                // Gom tất cả các Id có quyền tìm kiếm, đưa lên và yêu cầu sau đó xoá đi các Id đang trong quá trình ký
+                List<string> lsIdDisplay = new List<string>();
+
+                lsIdDisplay.AddRange(lsIsCanSearchs);
+                lsIdDisplay.AddRange(lsIdDocUserUpload);
+                lsIdDisplay.RemoveAll(r => lsIdBaseRemove.Contains(r));
+                lsIdDisplay = lsIdDisplay.Distinct().ToList();
+
+                lsKnowledgeBase = db.dt207_Base.Where(r => lsIdDisplay.Contains(r.Id) && !r.IsDelete).ToList();
             }
 
+            // Tạo danh sách các đối tượng DataDisplay bằng cách kết hợp lsKnowledgeBase, lsUsers, và lsKnowledgeTypes
+            // Chọn các thuộc tính cụ thể để điền vào các đối tượng DataDisplay
             var lsDataDisplays = (from data in lsKnowledgeBase
                                   join userUpload_ in lsUsers on data.UserUpload equals userUpload_.Id
                                   join userRequest_ in lsUsers on data.UserRequest equals userRequest_.Id
@@ -101,12 +162,19 @@ namespace KnowledgeSystem.Views._02_StandardsAndTechs._07_KnowledgeBase
                                       Keyword = IsSimple ? null : data.Keyword,
                                       UserUploadName = userUpload_.DisplayName,
                                       UploadDate = data.UploadDate
-                                  }).ToList();
+                                  })
+                                  .ToList();
 
+            // Đặt nguồn dữ liệu của sourceKnowledge là danh sách các đối tượng DataDisplay
             sourceKnowledge.DataSource = lsDataDisplays;
+
+            // Đặt tính hiển thị của cột gvColKeyword dựa trên giá trị của biến IsSimple
             gvColKeyword.Visible = !IsSimple;
 
+            // Điều chỉnh độ rộng các cột của gvData
             gvData.BestFitColumns();
+
+            // Tải thông tin liên quan đến giao diện
             helper.LoadViewInfo();
         }
 
@@ -119,10 +187,7 @@ namespace KnowledgeSystem.Views._02_StandardsAndTechs._07_KnowledgeBase
 
             gcData.DataSource = sourceKnowledge;
 
-            gvData.OptionsEditForm.ShowOnDoubleClick = DevExpress.Utils.DefaultBoolean.False;
-            gvData.OptionsEditForm.ShowOnEnterKey = DevExpress.Utils.DefaultBoolean.False;
-            gvData.OptionsEditForm.ShowOnF2Key = DevExpress.Utils.DefaultBoolean.False;
-            gvData.OptionsBehavior.EditingMode = GridEditingMode.EditFormInplace;
+            gvData.ReadOnlyGridView();
 
             LoadData();
         }
@@ -146,11 +211,12 @@ namespace KnowledgeSystem.Views._02_StandardsAndTechs._07_KnowledgeBase
 
         private void gcData_DoubleClick(object sender, EventArgs e)
         {
-            int forcusRow = gvData.FocusedRowHandle;
-            if (forcusRow < 0) return;
+            int focusedRow = gvData.FocusedRowHandle;
+            if (focusedRow < 0)
+                return;
 
-            DataDisplay dataRow = gvData.GetRow(forcusRow) as DataDisplay;
-            string idDocuments = dataRow.Id;
+            DataDisplay dataRow = gvData.GetRow(focusedRow) as DataDisplay;
+            string idDocuments = dataRow?.Id;
 
             f207_Document_Info fDocumentInfo = new f207_Document_Info(idDocuments);
             fDocumentInfo.ShowDialog();
