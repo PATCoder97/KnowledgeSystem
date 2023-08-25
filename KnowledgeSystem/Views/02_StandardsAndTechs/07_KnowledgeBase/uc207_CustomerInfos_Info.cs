59529f924af7428e3c151395938eee1ff08ea70d
AnhTuanDELL <Phananhtuan6@outlook.com>
2023-08-25 14:31:41 +0700

    Thêm form Thông tin khách hàng của guiji

---
+++ KnowledgeSystem/Views/02_StandardsAndTechs/07_KnowledgeBase/uc207_CustomerInfos_Info.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnowledgeSystem.Views._02_StandardsAndTechs._07_KnowledgeBase
+{
+    public partial class uc207_CustomerInfos_Info : DevExpress.XtraEditors.XtraUserControl
+    {
+        public uc207_CustomerInfos_Info()
+        {
+            InitializeComponent();
+        }
+    }
+}
