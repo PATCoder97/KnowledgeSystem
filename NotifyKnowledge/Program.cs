015b3358458a9f8526171448bf8720f78b316662
AnhTuanDELL <Phananhtuan6@outlook.com>
2023-09-07 10:41:24 +0700

    Thêm KnowledgeNotifyService

---
+++ NotifyKnowledge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotifyKnowledge
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new KnowledgeNotifyService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
