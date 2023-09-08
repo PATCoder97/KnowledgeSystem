9db41edf7164f2c4ac619510c5eace6d07b62b09
AnhTuanDELL <Phananhtuan6@outlook.com>
2023-09-08 11:01:11 +0700

    Service không nhận EF nên xoá

--- DataEF/Models/sys_Log.cs
+++ DataEF/Models/sys_Log.cs
@@ -15,7 +15,7 @@ namespace DataEF
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
-        public string Thread { get; set; }
+        public string ThreadName { get; set; }
         public string Level { get; set; }
         public string Logger { get; set; }
         public string Message { get; set; }
