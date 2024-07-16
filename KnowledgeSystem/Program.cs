9c6bc9ccf2679304fcb9bf25b478f6e6ab77f2e7
AnhTuanDELL <Phananhtuan6@outlook.com>
2024-07-16 10:29:33 +0700

    deploy 2024.07.16

--- KnowledgeSystem/Program.cs
+++ KnowledgeSystem/Program.cs
@@ -36,7 +36,7 @@ namespace KnowledgeSystem
         [STAThread]
         static void Main(string[] args)
         {
-            new AppCopyRight() { Version = "24.07.06" };
+            new AppCopyRight() { Version = "24.07.16" };
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             TPConfigs.SetSystemStaticValue();
