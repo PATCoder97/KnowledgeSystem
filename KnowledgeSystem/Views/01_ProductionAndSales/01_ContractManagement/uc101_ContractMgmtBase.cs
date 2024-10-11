44dfec9cbde4e691fabde187f483d189785abf41
AnhTuanDELL <Phananhtuan6@outlook.com>
2024-10-11 09:51:28 +0700

    [101] Tạo form quản lý hợp đồng

---
+++ KnowledgeSystem/Views/01_ProductionAndSales/01_ContractManagement/uc101_ContractMgmtBase.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnowledgeSystem.Views._01_ProductionAndSales._01_ContractManagement
+{
+    public partial class uc101_ContractMgmtBase : DevExpress.XtraEditors.XtraUserControl
+    {
+        public uc101_ContractMgmtBase()
+        {
+            InitializeComponent();
+        }
+    }
+}
