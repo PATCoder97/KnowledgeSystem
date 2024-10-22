6103379eaeef2ffa28fb679ee136d31068ce7c86
HA VY <HA VY@HAVY>
2024-10-22 18:30:04 +0700

    [308] tạo form Main

---
+++ KnowledgeSystem/Views/03_DepartmentManage/08_HealthCheckup/uc308_HealthCheckMain.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnowledgeSystem.Views._03_DepartmentManage._08_HealthCheckup
+{
+    public partial class uc308_HealthCheckMain : DevExpress.XtraEditors.XtraUserControl
+    {
+        public uc308_HealthCheckMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
