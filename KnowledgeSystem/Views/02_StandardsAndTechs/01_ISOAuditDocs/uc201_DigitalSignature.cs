c2e221614acf34a64c57f13e560a8f93ca486ad7
Phan Anh Tuấn <57442280+PATCoder97@users.noreply.github.com>
2024-10-23 07:29:49 +0700

    [201] Sửa giao diện ký điện tử

--- KnowledgeSystem/Views/02_StandardsAndTechs/01_ISOAuditDocs/uc201_DigitalSignature.cs
+++ KnowledgeSystem/Views/02_StandardsAndTechs/01_ISOAuditDocs/uc201_DigitalSignature.cs
@@ -1,14 +1,18 @@
 ﻿using BusinessLayer;
 using DataAccessLayer;
 using DevExpress.XtraEditors;
+using DevExpress.XtraGrid;
 using DevExpress.XtraGrid.Views.Grid;
 using KnowledgeSystem.Helpers;
+using KnowledgeSystem.Views._00_Generals;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,6 +24,16 @@ namespace KnowledgeSystem.Views._02_StandardsAndTechs._01_ISOAuditDocs
         public uc201_DigitalSignature()
         {
             InitializeComponent();
+            InitializeIcon();
+
+            gvData.MasterRowExpanded += (s, e) =>
+            {
+                GridView masterView = s as GridView;
+                int visibleDetailRelationIndex = masterView.GetVisibleDetailRelationIndex(e.RowHandle);
+                GridView detailView = masterView.GetDetailView(e.RowHandle, visibleDetailRelationIndex) as GridView;
+
+                detailView.BestFitColumns();
+            };
         }
 
         List<dt201_Forms> baseForm;
@@ -27,42 +41,90 @@ namespace KnowledgeSystem.Views._02_StandardsAndTechs._01_ISOAuditDocs
 
         BindingSource sourceForm = new BindingSource();
 
+        private void InitializeIcon()
+        {
+            btnAdd.ImageOptions.SvgImage = TPSvgimages.Add;
+            btnReload.ImageOptions.SvgImage = TPSvgimages.Reload;
+        }
+
         private void LoadData()
         {
+            List<dt201_Base> baseData = dt201_BaseBUS.Instance.GetList();
+
+
             baseForm = dt201_FormsBUS.Instance.GetListProcessing();
             users = dm_UserBUS.Instance.GetList();
 
             var dataInfo = (from data in baseForm
                             join usr in users on data.UploadUser equals usr.Id
+                            join category in baseData on data.IdBase equals category.Id
                             select new
                             {
+                                category,
                                 data,
                                 usr
+                            } into dt
+                            group dt by dt.category.IdParent into dtg
+                            select new
+                            {
+                                Key = dtg.Key,
+                                category = baseData.FirstOrDefault(r => r.Id == dtg.Key),
+                                detailData = dtg.Select(r => new
+                                {
+                                    UsrUploadName = $"{r.usr.Id.Substring(5)} {r.usr.DisplayName}",
+                                    data = r.data,
+                                    usr = r.usr
+                                }).ToList()
                             }).ToList();
 
             sourceForm.DataSource = dataInfo;
+            gcData.LevelTree.Nodes.Add("detailData", gvDetail);
 
             gvData.BestFitColumns();
-            gvData.ReadOnlyGridView();
-            gvData.KeyDown += GridControlHelper.GridViewCopyCellData_KeyDown;
+
+            if (gvData.RowCount > 0)
+                gvData.ExpandMasterRow(0, 0);
         }
 
         private void uc201_DigitalSignature_Load(object sender, EventArgs e)
         {
+            gvData.ReadOnlyGridView();
+            gvData.KeyDown += GridControlHelper.GridViewCopyCellData_KeyDown;
+            gvDetail.ReadOnlyGridView();
+            gvDetail.KeyDown += GridControlHelper.GridViewCopyCellData_KeyDown;
+            gvData.OptionsDetail.AllowOnlyOneMasterRowExpanded = true;
+
             gcData.DataSource = sourceForm;
             LoadData();
         }
 
         private void gvData_DoubleClick(object sender, EventArgs e)
         {
-            GridView view = sender as GridView;
+            int handle = gvData.FocusedRowHandle;
+            if (gvData.GetMasterRowExpanded(handle))
+            {
+                gvData.CollapseMasterRow(handle, 0);
+            }
+            else
+            {
+                gvData.ExpandMasterRow(handle, 0);
+            }
+        }
 
+        private void gvDetail_DoubleClick(object sender, EventArgs e)
+        {
+            GridView view = sender as GridView;
             int idForm = Convert.ToInt16(view.GetRowCellValue(view.FocusedRowHandle, gColId));
 
             f201_DocSignInfo fInfo = new f201_DocSignInfo();
             fInfo.idBaseForm = idForm;
             fInfo.ShowDialog();
 
+            LoadData();
+        }
+
+        private void btnReload_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
             LoadData();
         }
     }
