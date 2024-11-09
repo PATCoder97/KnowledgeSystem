286bb52aef1b354b6611f74f509d77ec78ec9d46
Phan Anh Tuấn <57442280+PATCoder97@users.noreply.github.com>
2024-11-09 14:35:05 +0700

    [201] Thêm chức năng thêm thay đổi nhân viên ISO bằng tay

--- KnowledgeSystem/Views/02_StandardsAndTechs/01_ISOAuditDocs/uc201_UpdateUsrReq.cs
+++ KnowledgeSystem/Views/02_StandardsAndTechs/01_ISOAuditDocs/uc201_UpdateUsrReq.cs
@@ -2,7 +2,9 @@
 using DataAccessLayer;
 using DevExpress.XtraEditors;
 using DevExpress.XtraGrid.Views.Grid;
+using DocumentFormat.OpenXml.Spreadsheet;
 using KnowledgeSystem.Helpers;
+using KnowledgeSystem.Views._04_SystemAdministrator._03_Extension;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -35,6 +37,7 @@ namespace KnowledgeSystem.Views._02_StandardsAndTechs._01_ISOAuditDocs
         {
             btnReload.ImageOptions.SvgImage = TPSvgimages.Reload;
             btnExportExcel.ImageOptions.SvgImage = TPSvgimages.Excel;
+            btnManualChange.ImageOptions.SvgImage = TPSvgimages.Add;
         }
 
         private void LoadData()
@@ -43,7 +46,14 @@ namespace KnowledgeSystem.Views._02_StandardsAndTechs._01_ISOAuditDocs
             users = dm_UserBUS.Instance.GetList();
             depts = dm_DeptBUS.Instance.GetList();
 
-            var dataInfo = (from data in baseReq
+            // Check quyền truy cập của tổ nào theo quyền của nhóm ISO đó
+            var grpUsrs = dm_GroupUserBUS.Instance.GetListByUID(TPConfigs.LoginUser.Id);
+            var groups = dm_GroupBUS.Instance.GetListByName("ISO組");
+            var idDeptGroups = (from data in groups
+                                join grp in grpUsrs on data.Id equals grp.IdGroup
+                                select data.IdDept).ToList();
+
+            var dataInfo = (from data in baseReq.Where(r => idDeptGroups.Contains(r.IdDept))
                             join usr in users on data.IdUsr equals usr.Id
                             join dept in depts on data.IdDept equals dept.Id
                             select new
@@ -90,6 +100,38 @@ namespace KnowledgeSystem.Views._02_StandardsAndTechs._01_ISOAuditDocs
 
             gcData.ExportToXlsx(filePath);
             Process.Start(filePath);
+        }
+
+        private void btnManualChange_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            uc201_UpdateUsrReq_Manual ucInfo = new uc201_UpdateUsrReq_Manual();
+            if (XtraDialog.Show(ucInfo, "手動創建", MessageBoxButtons.OKCancel) != DialogResult.OK)
+                return;
+
+            string userId = ucInfo.UserId;
+            string desscription = ucInfo.Description;
+
+            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(desscription))
+            {
+                XtraMessageBox.Show("請填寫所以訊息！", TPConfigs.SoftNameTW, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            var usr = dm_UserBUS.Instance.GetItemById(userId);
+
+            if (XtraMessageBox.Show($"是否需要創建「{usr.DisplayName}」到ISO 17025「{(desscription == "新增" ? "新進" : "離職")}人員」之關係表單？", TPConfigs.SoftNameTW, MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+                return;
+
+            dt201_UpdateUsrReq updateReq = new dt201_UpdateUsrReq()
+            {
+                IdUsr = userId,
+                IdDept = usr.IdDepartment,
+                DateCreate = DateTime.Now,
+                TypeChange = desscription == "新增" ? "新進" : "離職",
+                Describe = $"手動創建ISO 17025「{(desscription == "新增" ? "新進" : "離職")}人員」之關係表單"
+            };
+            dt201_UpdateUsrReqBUS.Instance.Add(updateReq);
+
+            LoadData();
         }
     }
 }
---
+++ KnowledgeSystem/Views/02_StandardsAndTechs/01_ISOAuditDocs/uc201_UpdateUsrReq_Manual.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer;
+using DevExpress.XtraEditors;
+using KnowledgeSystem.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnowledgeSystem.Views._02_StandardsAndTechs._01_ISOAuditDocs
+{
+    public partial class uc201_UpdateUsrReq_Manual : DevExpress.XtraEditors.XtraUserControl
+    {
+        public uc201_UpdateUsrReq_Manual()
+        {
+            InitializeComponent();
+
+            var users = dm_UserBUS.Instance.GetList().Where(r => r.Status == 0 && r.IdDepartment == TPConfigs.LoginUser.IdDepartment).ToList();
+            txbUser.Properties.DataSource = users;
+            txbUser.Properties.DisplayMember = "DisplayName";
+            txbUser.Properties.ValueMember = "Id";
+
+            txbEventChange.Properties.Items.AddRange(new object[] { "新增", "取消" });
+
+            txbUser.DataBindings.Add("EditValue", this, "UserId", false, DataSourceUpdateMode.OnPropertyChanged);
+            txbEventChange.DataBindings.Add("Text", this, "Description", false, DataSourceUpdateMode.OnPropertyChanged);
+        }
+
+        public string UserId { get; set; }
+        public string Description { get; set; }
+    }
+}
