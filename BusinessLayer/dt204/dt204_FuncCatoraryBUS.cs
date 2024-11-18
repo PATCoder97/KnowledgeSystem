ee0e51362323e6223337472d51862966c611b86d
Phan Anh Tuấn <57442280+PATCoder97@users.noreply.github.com>
2024-11-18 09:02:45 +0700

    [204] Create DB

---
+++ BusinessLayer/dt204/dt204_FuncCatoraryBUS.cs
@@ -0,0 +1,133 @@
+﻿using DataAccessLayer;
+using Logger;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class dt204_FuncCatoraryBUS
+    {
+        TPLogger logger;
+
+        private static dt204_FuncCatoraryBUS instance;
+
+        public static dt204_FuncCatoraryBUS Instance
+        {
+            get { if (instance == null) instance = new dt204_FuncCatoraryBUS(); return instance; }
+            private set { instance = value; }
+        }
+
+        private dt204_FuncCatoraryBUS() { logger = new TPLogger(MethodBase.GetCurrentMethod().DeclaringType.FullName); }
+
+        public List<dt204_FuncCatorary> GetList()
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    return _context.dt204_FuncCatorary.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                throw;
+            }
+        }
+
+        public dt204_FuncCatorary GetItemById(int id)
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    return _context.dt204_FuncCatorary.FirstOrDefault(r => r.Id == id);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                throw;
+            }
+        }
+
+        public bool Add(dt204_FuncCatorary item)
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    _context.dt204_FuncCatorary.Add(item);
+                    int affectedRecords = _context.SaveChanges();
+                    return affectedRecords > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                return false;
+            }
+        }
+
+        public bool AddRange(List<dt204_FuncCatorary> items)
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    _context.dt204_FuncCatorary.AddRange(items);
+                    int affectedRecords = _context.SaveChanges();
+                    return affectedRecords > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                return false;
+            }
+        }
+
+        public bool AddOrUpdate(dt204_FuncCatorary item)
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    _context.dt204_FuncCatorary.AddOrUpdate(item);
+                    int affectedRecords = _context.SaveChanges();
+                    return affectedRecords > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                return false;
+            }
+        }
+
+        public bool RemoveById(int id)
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    var itemRemove = _context.dt204_FuncCatorary.FirstOrDefault(r => r.Id == id);
+                    _context.dt204_FuncCatorary.Remove(itemRemove);
+
+                    int affectedRecords = _context.SaveChanges();
+                    return affectedRecords > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                throw;
+            }
+        }
+    }
+}
---
+++ BusinessLayer/dt204/dt204_InternalDocMgmtBUS.cs
@@ -0,0 +1,136 @@
+﻿using DataAccessLayer;
+using Logger;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class dt204_InternalDocMgmtBUS
+    {
+        TPLogger logger;
+
+        private static dt204_InternalDocMgmtBUS instance;
+
+        public static dt204_InternalDocMgmtBUS Instance
+        {
+            get { if (instance == null) instance = new dt204_InternalDocMgmtBUS(); return instance; }
+            private set { instance = value; }
+        }
+
+        private dt204_InternalDocMgmtBUS() { logger = new TPLogger(MethodBase.GetCurrentMethod().DeclaringType.FullName); }
+
+        public List<dt204_InternalDocMgmt> GetList()
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    return _context.dt204_InternalDocMgmt.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                throw;
+            }
+        }
+
+        public dt204_InternalDocMgmt GetItemById(int id)
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    return _context.dt204_InternalDocMgmt.FirstOrDefault(r => r.Id == id);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                throw;
+            }
+        }
+
+        public bool Add(dt204_InternalDocMgmt item)
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    _context.dt204_InternalDocMgmt.Add(item);
+                    int affectedRecords = _context.SaveChanges();
+                    return affectedRecords > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                return false;
+            }
+        }
+
+        public bool AddRange(List<dt204_InternalDocMgmt> items)
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    _context.dt204_InternalDocMgmt.AddRange(items);
+                    int affectedRecords = _context.SaveChanges();
+                    return affectedRecords > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                return false;
+            }
+        }
+
+        public bool AddOrUpdate(dt204_InternalDocMgmt item)
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    _context.dt204_InternalDocMgmt.AddOrUpdate(item);
+                    int affectedRecords = _context.SaveChanges();
+                    return affectedRecords > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                return false;
+            }
+        }
+
+        public bool RemoveById(int id, string idUsrDel)
+        {
+            try
+            {
+                using (var _context = new DBDocumentManagementSystemEntities())
+                {
+                    var itemRemove = _context.dt204_InternalDocMgmt.FirstOrDefault(r => r.Id == id);
+                    itemRemove.IsDel = true;
+                    itemRemove.DelTime = DateTime.Now;
+                    itemRemove.IdUsrDel = idUsrDel;
+
+                    _context.dt204_InternalDocMgmt.AddOrUpdate(itemRemove);
+                    int affectedRecords = _context.SaveChanges();
+                    return affectedRecords > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(MethodBase.GetCurrentMethod().ReflectedType.Name, ex.ToString());
+                return false;
+            }
+        }
+    }
+}
