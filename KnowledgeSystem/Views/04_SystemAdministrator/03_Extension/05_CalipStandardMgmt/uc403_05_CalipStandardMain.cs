1d1041a123f19bad686b08f2182165e4cdfa9e13
Dell Alpha <Dell Alpha@DELL-ALPHA-01>
2025-12-09 14:00:32 +0700

    [403-05] Start

---
+++ KnowledgeSystem/Views/04_SystemAdministrator/03_Extension/05_CalipStandardMgmt/uc403_05_CalipStandardMain.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnowledgeSystem.Views._04_SystemAdministrator._03_Extension._05_CalipStandardMgmt
+{
+    public partial class uc403_05_CalipStandardMain : DevExpress.XtraEditors.XtraUserControl
+    {
+        public uc403_05_CalipStandardMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
