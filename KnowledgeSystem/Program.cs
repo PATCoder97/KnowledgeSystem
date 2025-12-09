d1c80f69d1f80e8997aaa5139ad7da6ddc6aafce
Dell Alpha <Dell Alpha@DELL-ALPHA-01>
2025-12-09 13:05:01 +0700

    deploy 25.12.09

--- KnowledgeSystem/Program.cs
+++ KnowledgeSystem/Program.cs
@@ -39,7 +39,7 @@ namespace KnowledgeSystem
         [STAThread]
         static void Main(string[] args)
         {
-            new AppCopyRight() { Version = "25.11.21" };
+            new AppCopyRight() { Version = "25.12.09" };
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             TPConfigs.SetSystemStaticValue();
