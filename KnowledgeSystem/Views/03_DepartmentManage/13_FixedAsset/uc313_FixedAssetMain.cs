a78984b7b88d02aec388e51ab34371ba7e1800d7
tuan <tuan@fhs.local>
2026-03-28 08:48:43 +0700

    [313] init fixed asset

---
+++ KnowledgeSystem/Views/03_DepartmentManage/13_FixedAsset/uc313_FixedAssetMain.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnowledgeSystem.Views._03_DepartmentManage._13_FixedAsset
+{
+    public partial class uc313_FixedAssetMain : DevExpress.XtraEditors.XtraUserControl
+    {
+        public uc313_FixedAssetMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
